6b8ccc9e9cb28e53e069539eae875701493c5109
zhumxiang <zmx.jlife@gmail.com>
2021-01-30 16:30:51 +0800

    v12

--- src/egret/workbench/parts/about/aboutPanel.tsx
+++ src/egret/workbench/parts/about/aboutPanel.tsx
@@ -9,7 +9,7 @@ import { localize } from '../../../base/localization/nls';
 import { APPLICATION_NAME } from 'egret/consts/consts';
 
 const customAuther = "zmx";
-const customVersion = 11;
+const customVersion = 12;
 /**
  * 新建文件夹
  * @export
